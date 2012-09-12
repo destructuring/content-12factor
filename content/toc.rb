76bb892519cabc10a309ecb3befe7fe2a402ecc6
Tom Bombadil <amanibhavam@destructuring.org>
2012-09-11 21:08:23 -0700

    toc also needs .html

--- content/toc.rb
+++ content/toc.rb
@@ -1,1 +1,1 @@
-TOC = %w(codebase dependencies config backing-services build-release-run processes port-binding concurrency disposability dev-prod-parity logs admin-processes)
+TOC = %w(codebase dependencies config backing-services build-release-run processes port-binding concurrency disposability dev-prod-parity logs admin-processes).collect {|c| "#{c}.html" }
